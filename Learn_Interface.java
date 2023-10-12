70bb3358b0cb5cf8d7e5510242dc989471f45f90
Vinay Patel <vinaypatel5519@gmai.com>
2023-10-12 17:00:40 +0530

    More On Interface

--- Learn_Interface.java
+++ Learn_Interface.java
@@ -9,12 +9,12 @@
     ** It cannot have a method body. **
  */
 
- /*
-    Why use Java interface?
+/*
+   Why use Java interface?
 
-    1) It is used to achieve abstraction.
-    2) By interface, we can support the functionality of multiple inheritance.
-    3) It can be used to achieve loose coupling.
+   1) It is used to achieve abstraction.
+   2) By interface, we can support the functionality of multiple inheritance.
+   3) It can be used to achieve loose coupling.
 
 */
 
@@ -33,9 +33,11 @@
 
 */
 
+/*
 interface printable{
     void print();
 }
+
 
 class Learn_Interface implements printable{
     public void print(){
@@ -47,4 +49,76 @@ class Learn_Interface implements printable{
         obj.print();
     }
 
+}
+
+*/
+
+/*
+ -> In a real scenario, an interface is defined by someone else, 
+ -> but its implementation is provided by different implementation providers. -> Moreover, it is used by someone else. 
+ ->The implementation part is hidden by the user who uses the interface.
+
+*/
+
+/* 
+
+// Interface declartion : by 1st user
+interface Drawable {
+    void draw();
+}
+
+// Implementation : by second user
+
+class Rectangle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing reactangle ");
+    }
+}
+
+class Circle implements Drawable {
+    public void draw() {
+        System.out.println("Drawing circle..");
+    }
+}
+
+// Using interface : by 3rd user
+
+public class Learn_Interface {
+    public static void main(String[] args) {
+        Drawable d = new Circle();
+        d.draw();
+    }
+}
+
+*/
+
+// Multiple inheritance in Java by interface
+// If a class implements multiple interfaces, or an interface extends multiple interfaces, it is known as multiple inheritance.
+
+// Q) Multiple inheritance is not supported through class in java, but it is possible by an interface, why?
+// Ans :- because of ambiguity . However, it is supported in case of an interface because there is no ambiguity. It is because its implementation is provided by the implementation class.
+
+// Example
+
+interface printable {
+    void print();
+}
+
+interface showable {
+    void show();
+}
+
+public class Learn_Interface implements printable, showable {
+    public void print() {
+        System.out.println("Hello");
+    }
+
+    public void show() {
+        System.out.println("Welcome");
+    }
+
+    public static void main(String[] args) {
+        Learn_Interface obj = new Learn_Interface();
+        obj.show();
+    }
 }
